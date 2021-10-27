a91159e7cfadacdb9ac38f5c26d2c70c29dfe8c0
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-27 18:29:44 +0200

    adds movies to db

--- AlloCine/db/seeds.rb
+++ AlloCine/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+Movie.destroy_all
+
+100.times do |i|
+  Movie.create(
+    name: Faker::Movie.title,
+    year: Faker::Number.within(range: 1900..2020),
+    genre: ['action', 'horreur', 'comédie', 'drame'][rand(0..3)],
+    synopsis: Faker::Lorem.sentence(word_count: 10, supplemental: true, random_words_to_add: 3),
+    director: Faker::Name.name,
+    allocine_rating: Faker::Number.within(range: 0.0..5.0).truncate(1),
+    my_rating: nil,
+    already_seen: false
+  )
+end
